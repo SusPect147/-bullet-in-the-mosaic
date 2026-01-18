3782a9709ed00b76d44fa2758ffb5b2877350600
SusPect147 <p.a.chmykhalov@gmail.com>
2026-01-18 17:25:49 +0300

    Сделал стороны будущего лабиринта. Планирую сверху добавить счет и кнопку выйти

--- script.py
+++ script.py
@@ -2,62 +2,93 @@ import arcade
 
 SCREEN_WIDTH = 610
 SCREEN_HEIGHT = 610
-WINDOW_TITLE = "Приветствие"
+WINDOW_TITLE = "Maze Prototype"
 
-class WelcomeWindow(arcade.Window):
+BACKGROUND_COLOR = arcade.color.BLACK
+WALL_COLOR = arcade.color.GRAY
+
+
+class GameWindow(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
-        arcade.set_background_color(arcade.color.DARK_SLATE_GRAY)
+        arcade.set_background_color(BACKGROUND_COLOR)
 
-        self.welcome_text = "Добро пожаловать, User"
         self.show_welcome = True
+
+        # Список стен
+        self.walls = arcade.SpriteList()
+        self.create_maze_contour()
+
+    def create_maze_contour(self):
+        # Левая стена
+        left_wall = arcade.SpriteSolidColor(10, 600, WALL_COLOR)
+        left_wall.center_x = 5
+        left_wall.center_y = SCREEN_HEIGHT / 2
+
+        # Правая стена
+        right_wall = arcade.SpriteSolidColor(10, 600, WALL_COLOR)
+        right_wall.center_x = SCREEN_WIDTH - 5
+        right_wall.center_y = SCREEN_HEIGHT / 2
+
+        # Нижняя стена
+        bottom_wall = arcade.SpriteSolidColor(600, 10, WALL_COLOR)
+        bottom_wall.center_x = SCREEN_WIDTH / 2
+        bottom_wall.center_y = 5
+
+        # Верхняя стена
+        top_wall = arcade.SpriteSolidColor(600, 10, WALL_COLOR)
+        top_wall.center_x = SCREEN_WIDTH / 2
+        top_wall.center_y = SCREEN_HEIGHT - 5
+
+        self.walls.extend([
+            left_wall,
+            right_wall,
+            bottom_wall,
+            top_wall
+        ])
 
     def on_draw(self):
         self.clear()
 
         if self.show_welcome:
-            arcade.draw_text(
-                self.welcome_text,
-                self.width / 2,
-                self.height / 2,
-                arcade.color.WHITE,
-                36,
-                anchor_x="center",
-                anchor_y="center",
-            )
-
-            arcade.draw_text(
-                "Нажмите любую клавишу или клик мышью",
-                self.width / 2,
-                self.height / 2 - 60,
-                arcade.color.LIGHT_GRAY,
-                14,
-                anchor_x="center",
-                anchor_y="center",
-            )
+            self.draw_welcome_screen()
         else:
-            arcade.draw_text(
-                "Основной экран",
-                self.width / 2,
-                self.height / 2,
-                arcade.color.WHITE,
-                24,
-                anchor_x="center",
-                anchor_y="center",
-            )
+            self.walls.draw()
+
+    def draw_welcome_screen(self):
+        arcade.draw_text(
+            "Добро пожаловать, User",
+            SCREEN_WIDTH / 2,
+            SCREEN_HEIGHT / 2,
+            arcade.color.WHITE,
+            36,
+            anchor_x="center",
+            anchor_y="center",
+        )
+
+        arcade.draw_text(
+            "Нажмите любую клавишу или клик мышью",
+            SCREEN_WIDTH / 2,
+            SCREEN_HEIGHT / 2 - 60,
+            arcade.color.LIGHT_GRAY,
+            14,
+            anchor_x="center",
+            anchor_y="center",
+        )
 
     def on_key_press(self, symbol, modifiers):
-        if symbol == arcade.key.ESCAPE:
-            self.close()
-        else:
+        if self.show_welcome:
             self.show_welcome = False
+        elif symbol == arcade.key.ESCAPE:
+            self.close()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        self.show_welcome = False
+        if self.show_welcome:
+            self.show_welcome = False
 
 
 def main():
-    window = WelcomeWindow()
+    GameWindow()
     arcade.run()
 
 
